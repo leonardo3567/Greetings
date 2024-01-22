759b832c81b4ca6f60196fa7065dce2d95394bb5
Leonardo Gomes <leonardo35671@hotmail.com>
2024-01-22 00:30:25 +0000

    Initial Commit

---
+++ Greetings/GreetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreetingsApp.swift
+//  Greetings
+//
+//  Created by Newuser on 22/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct GreetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
